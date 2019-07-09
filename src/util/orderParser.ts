be749fdbba575de8c5dbb7d387a63ab7ade90f5e
Kevin Kelly <kevin@kevinjkelly.net>
2019-07-09 03:36:10 -0400

    cleanup deps

--- src/util/orderParser.ts
+++ src/util/orderParser.ts
@@ -1,4 +1,4 @@
-import { isString } from "lodash";
+import { isString } from "util";
 import { parseTime } from "./parseTime";
 import { parseCoordinates, Coordinates } from "./parseCoordinates";
 
--- src/util/parseCoordinates.ts
+++ src/util/parseCoordinates.ts
@@ -1,4 +1,4 @@
-import { isString } from "lodash";
+import { isString } from "util";
 
 /**
  * Coordinate data type.
